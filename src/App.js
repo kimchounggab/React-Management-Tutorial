138a70c830725ea34f2885e5f0ebc6a1912340ab
kcg <lyco0107@gmail.com>
2021-02-19 09:42:25 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="gray-background">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2> Let's develope mangement system</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
